8c1f7663ac9fd7fcc8f491ab856f91f5ca8b228a
Chris <chrdoan@umass.edu>
2019-11-17 19:58:37 -0500

    added working form field study buddy

--- study_buddy/background.js
+++ study_buddy/background.js
@@ -1,10 +1,32 @@
 //the chrome extensions event handler: constantly listens for browser events
-  chrome.webRequest.onBeforeRequest.addListener(
-    function() {
-        return {cancel: true,};
-    },
-    {
-        urls: ["https://*.twitter.com/*",]
-    },
-    ["blocking"]
-);
+var webs = ["https://*.reddit.com/*"];
+
+chrome.storage.sync.set({'urls': webs}, function() {
+    console.log('Value is set to ' + webs);
+  });
+
+  chrome.storage.onChanged.addListener(function() {
+    chrome.storage.sync.get('urls', function(result) {
+    var webs = result.urls
+    chrome.webRequest.onBeforeRequest.addListener(function() {
+      return {cancel: true,};
+      },
+      {
+      urls: webs
+      },
+      ["blocking"]
+  );
+    });
+});
+
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {urlContains: 'http'},
+      })
+      ],
+          actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+
+
--- study_buddy/popup.js
+++ study_buddy/popup.js
@@ -1,2 +1,24 @@
-let onButton = documentt.getElemenById('onButton');
-
+function message(val){
+    $(".message").text(val);
+  }
+  
+    //handle update
+    $("#urlbutton").click(function(){
+        chrome.storage.sync.get('urls', function(result) { 
+            var update = result.urls;
+            update = update.concat('https://*.'+ $("#urlinput").val()+'.com/*' )  
+            console.log(update[update.length-1])
+            chrome.storage.sync.set({'urls': update}, function() {
+                // Notify that we saved.
+                console.log("update!")
+          });
+        }); 
+    });
+  
+    //allow update when enter is pressed
+    $('input').keypress(function (e) {
+      if (e.which == 13) {
+        $(".urlbutton").click();
+        return false;
+      }
+    });
